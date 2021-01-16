e35ac0a5f52948dee35805bb2f6b167f8507e7b0
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2021-01-16 20:09:45 +0100

    done

---
+++ src/Pages/Register/components/form.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Formik, /*useFormikContext*/ } from "formik";
+import { Input, Form, SubmitButton } from 'formik-antd';
+import { UserOutlined, MailOutlined } from '@ant-design/icons';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { Alert, Button, Space, Row, Col, Typography } from "antd";
+
+
+
+const RegisterForm = (props) => {
+    return (
+        <Formik
+        initialValues={props.initialValues}
+        validate={props.formValidate}
+        onSubmit={props.handleFormSubmit}
+        validateOnMount={true}
+        >
+        {({
+            isValid,
+            handleSubmit,
+        }) => (
+            <Row align="middle" justify="center" style={{height: "100vh"}}>
+                <Col xs={{ span: 24}} md={{ span:12 }} lg={{ span: 8}}>
+                    <Form onSubmit={{handleSubmit}} style={{padding: "2rem"}}>
+                        <Alert
+                            message={props.feedBack ? props.feedBack.message : ""}
+                            type={props.feedBack ? props.feedBack.type : ""}
+                            showIcon
+                            style={{marginBottom: "1.5rem", display: !props.feedBack.message ? "None" : ""}}
+                        />
+                        <Typography.Title level={4} style={{textAlign: "center", marginBottom: "2rem"}}>Log in</Typography.Title>
+                        <Row gutter={[24, 24]}>
+                            <Col xs={{ span: 24}} md={{ span:24 }} lg={{ span: 24}}>
+                                <Input name="username" type="text" placeholder="Enter username" size="large" prefix={<UserOutlined />} />
+                            </Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                            <Col xs={{ span: 24}} md={{ span:24 }} lg={{ span: 24}}>
+                                <Input name="email" type="email" placeholder="Enter email" size="large" prefix={<MailOutlined />} />
+                            </Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                            <Col xs={{ span: 24}} md={{ span:24 }} lg={{ span: 24}}>
+                                <Input 
+                                    name="password" 
+                                    type="password" 
+                                    placeholder="Enter password" 
+                                    size="large" 
+                                    prefix={<EyeInvisibleOutlined />}
+                                />
+                            </Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                            <Col xs={{ span: 24 }} md={{ span: 24 }} lg={{ span: 24 }}>
+                            <SubmitButton
+                                style={{backgroundColor: "#4287f5", color: "white"}}
+                                block
+                                disabled={!isValid}
+                                size="large"
+                            >
+                                Register
+                            </SubmitButton>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row> 
+        )}
+        </Formik>
+    )
+};
+
+export default RegisterForm;
---
+++ src/Pages/Register/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import RegisterForm from "./components/form";
+
+
+
+const Register = () => {
+
+    const initialValues = {
+        email: "",
+        password: "",
+        username: ""
+    }
+
+    const [ feedBack, setFeedBack ] = useState({}) 
+
+    const formValidate = values => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+            errors.email = 'Invalid email address';
+        } else if (values.password==="") {
+            errors.password = "Password is Required"
+        } else if (values.username==="") {
+            errors.username = "Password is Required"
+        }
+        return errors;
+    }
+
+    const handleFormSubmit = values => {
+        console.log(values);
+        setFeedBack({message: "Registration Successfull", type: "success"});
+        return true
+    }
+    return(
+        <RegisterForm 
+            initialValues={initialValues}
+            formValidate={formValidate}
+            handleFormSubmit={handleFormSubmit}
+            feedBack={feedBack}
+        />
+    )
+};
+
+export default Login;
