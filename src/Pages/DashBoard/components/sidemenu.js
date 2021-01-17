a33562b82889cb057ad123b6f612e17572f61b70
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2021-01-17 23:27:14 +0100

    done

--- src/Pages/DashBoard/components/sidemenu.js
+++ src/Pages/DashBoard/components/sidemenu.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { Menu, Button } from 'antd';
 import {
-  AppstoreOutlined,
+  SettingOutlined,
   MenuUnfoldOutlined,
   MenuFoldOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
+  ScheduleOutlined,
+  LogoutOutlined,
+  KeyOutlined,
+  UserOutlined,
+  TagsOutlined
 } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 
-export const SideMenu = () => {
+const SideMenu = () => {
     
     const [ toggleState, setToggleState ] = useState(true)
 
@@ -23,7 +24,7 @@ export const SideMenu = () => {
     return (
       <div style={{ width: 256 }}>
         <Button type="primary" onClick={handleClick} style={{ marginBottom: 16 }}>
-          {React.createElement(this.toggleState ? MenuUnfoldOutlined : MenuFoldOutlined)}
+          {React.createElement(toggleState ? MenuUnfoldOutlined : MenuFoldOutlined)}
         </Button>
         <Menu
           defaultSelectedKeys={['1']}
@@ -31,31 +32,21 @@ export const SideMenu = () => {
           mode="inline"
           theme="dark"
           inlineCollapsed={toggleState}
+          style={{height: "100vh"}}
         >
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<ContainerOutlined />}>
-            Option 3
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
+          <SubMenu key="sub1" icon={<ScheduleOutlined />} title="Projects">
+            <Menu.Item key="5" icon={<TagsOutlined />}>E-Commerce App</Menu.Item>
+            <Menu.Item key="6" icon={<TagsOutlined />}>Trading Bot</Menu.Item>
+            <Menu.Item key="7" icon={<TagsOutlined />}>Telegram Bot</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
+          <SubMenu key="sub2" icon={<SettingOutlined />} title="Account">
+            <Menu.Item key="9" icon={<UserOutlined />}>Profile</Menu.Item>
+            <Menu.Item key="10" icon={<KeyOutlined />}>Password Change</Menu.Item>
+            <Menu.Item key="11" icon={<LogoutOutlined />}>Logout</Menu.Item>
           </SubMenu>
         </Menu>
       </div>
     );
 };
+
+export default SideMenu;
--- src/Pages/DashBoard/index.js
+++ src/Pages/DashBoard/index.js
@@ -16,7 +16,6 @@ const DashBoard = () => {
 
     return (
         <>
-            <Nav />
             <SideMenu />
         </>
     )
