22b64401efd3fb6f8f6ea375b1cd42eff620ef55
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2021-01-23 17:26:40 +0100

    --

--- src/Pages/DashBoard/components/createlist.js
+++ src/Pages/DashBoard/components/createlist.js
@@ -7,12 +7,15 @@ import { isEmptyArray } from 'formik';
 
 const CreateListModal = (props) => {
 
-  const [ modalState, setModalState ] = useState({buttonState: false, currentName: ""});
+  const [ modalState, setModalState ] = useState({buttonState: false, currentName: "", isLoading: false});
 
   const changeCurrentValue = (value) => {
-      if (!isEmptyArray(value)){
-        setModalState({buttonState: true, currentName: value});
-      }
+    setModalState({buttonState: !!value, currentName: value, isLoading: modalState.isLoading});
+  }
+
+  const createList = () => {
+      console.log(modalState.currentName);
+      setModalState({buttonState: modalState.buttonState, currentName: modalState.currentName, isLoading: true});
   }
   
   return (
@@ -25,7 +28,13 @@ const CreateListModal = (props) => {
         centered
       >
         <Input size="large" onChange={(e)=>changeCurrentValue(e.currentTarget.value)} placeholder="Project Name" prefix={<ProjectOutlined />} />
-        <Button type="primary" block style={{marginTop: "1rem"}} disabled={!modalState.buttonState}>
+        <Button 
+          type="primary" 
+          block style={{marginTop: "1rem"}} 
+          disabled={!modalState.buttonState}
+          onClick={createList}
+          loading={modalState.isLoading}
+        >
             Submit
         </Button>
       </Modal>
